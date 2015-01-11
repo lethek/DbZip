379ed647b9f9319f8c5b13bae7fc7421ac6b5afa
Michael Monsour <michael@lethek.net>
2015-01-11 14:46:16 +1000

    switched to using SeriLog instead of NLog

--- Program.cs
+++ Program.cs
@@ -3,13 +3,11 @@ using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
-
 using CommandLine;
-
 using DbZip.Tasks;
 using DbZip.Threading;
-
-using NLog;
+using Serilog;
+using Serilog.Events;
 
 using SevenZip;
 
@@ -22,6 +20,12 @@ namespace DbZip
 
 		public static void Main(string[] args)
 		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.WriteTo.ColoredConsole(LogEventLevel.Information, "{Message}{NewLine}")
+				.WriteTo.RollingFile(@"Logs\Log.{Date}.txt")
+				.CreateLogger();
+
 			try {
 				//Set base-priority of the process so it hopefully doesn't interfere too much with SQL Server
 				Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.BelowNormal;
@@ -49,7 +53,7 @@ namespace DbZip
 					//BACKUP DATABASE
 					string backupFileName;
 					using (new GlobalMutex(timeout: (options.Wait ? Timeout.Infinite : 0))) {
-						Log.Info("Backing up: [{0}].[{1}] ({2})", builder.DataSource, options.Database, options.TransactionLogBackup ? "TRANSACTION-LOG" : "FULL");
+						Log.Information("Backing up: [{0}].[{1}] ({2})", builder.DataSource, options.Database, options.TransactionLogBackup ? "TRANSACTION-LOG" : "FULL");
 						timer.Start();
 
 						var backupTask = new BackupTask(
@@ -59,67 +63,67 @@ namespace DbZip
 							statementTimeout: (int)TimeSpan.FromHours(4).TotalSeconds
 						);
 
-						if (Log.IsDebugEnabled) {
+						if (Log.IsEnabled(LogEventLevel.Debug)) {
 							backupTask.Progress += (sender, eventArgs) => Log.Debug(eventArgs.Message);
 						}
 
 						backupFileName = backupTask.Run();
 
 						timer.Stop();
-						Log.Info("Backed up in {0} ms", timer.ElapsedMilliseconds);
+						Log.Information("Backed up in {0} ms", timer.ElapsedMilliseconds);
 					}
 
 
 					if (options.SevenZip) {
 						//ARCHIVE DATABASE BACKUP
-						Log.Info("Zipping up: {0}", backupFileName);
+						Log.Information("Zipping up: {0}", backupFileName);
 						timer.Restart();
 						string archiveFileName = new SevenZipTask(backupFileName, CompressionLevel.Low).Run();
 						timer.Stop();
-						Log.Info("Zipped up in {0} ms", timer.ElapsedMilliseconds);
+						Log.Information("Zipped up in {0} ms", timer.ElapsedMilliseconds);
 
 						//VERIFY ARCHIVE AND CLEANUP
-						Log.Info("Verifying: {0}", archiveFileName);
+						Log.Information("Verifying: {0}", archiveFileName);
 						timer.Restart();
 						bool isValid = SevenZipTask.Verify(archiveFileName);
 						timer.Stop();
-						Log.Info("Verification {0} in {1} ms", isValid ? "passed" : "failed", timer.ElapsedMilliseconds);
+						Log.Information("Verification {0} in {1} ms", isValid ? "passed" : "failed", timer.ElapsedMilliseconds);
 						if (isValid) {
 							if (File.Exists(backupFileName)) {
-								Log.Info("Deleting {0}", backupFileName);
+								Log.Information("Deleting {0}", backupFileName);
 								File.Delete(backupFileName);
 							}
 						}
 
 					} else {
 						//ARCHIVE DATABASE BACKUP
-						Log.Info("Zipping up: {0}", backupFileName);
+						Log.Information("Zipping up: {0}", backupFileName);
 						timer.Restart();
 						string archiveFileName = new ZipTask(backupFileName).Run();
 						timer.Stop();
-						Log.Info("Zipped up in {0} ms", timer.ElapsedMilliseconds);
+						Log.Information("Zipped up in {0} ms", timer.ElapsedMilliseconds);
 
 						//VERIFY ARCHIVE AND CLEANUP
-						Log.Info("Verifying: {0}", archiveFileName);
+						Log.Information("Verifying: {0}", archiveFileName);
 						timer.Restart();
 						bool isValid = ZipTask.Verify(archiveFileName);
 						timer.Stop();
-						Log.Info("Verification {0} in {1} ms", isValid ? "passed" : "failed", timer.ElapsedMilliseconds);
+						Log.Information("Verification {0} in {1} ms", isValid ? "passed" : "failed", timer.ElapsedMilliseconds);
 						if (isValid) {
 							if (File.Exists(backupFileName)) {
-								Log.Info("Deleting {0}", backupFileName);
+								Log.Information("Deleting {0}", backupFileName);
 								File.Delete(backupFileName);
 							}
 						}
 					}
 
 
-					Log.Info("Completed");
-					Log.Info("-------------------------------------------------------------------------------");
+					Log.Information("Completed");
+					Log.Debug("-------------------------------------------------------------------------------");
 
 				} catch (TimeoutException) {
 					const string message = "Another backup is already in progress";
-					Log.Trace(message);
+					Log.Verbose(message);
 					Console.Error.WriteLine(message);
 				}
 
@@ -135,8 +139,6 @@ namespace DbZip
 
 
 		private const int ERROR_BAD_ARGUMENTS = 160;
-
-		private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
 	}
 
